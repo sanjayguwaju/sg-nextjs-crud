ed4e39b902fd899db18fee321d3a57c96dca38df
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 09:37:33 +0545

    created Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link href={"/"}>GT Coding</Link>
-            <Link href={"/addTopic"}>Add Topic</Link>
+        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+            <Link className="text-white font-bold" href={"/"}>GT Coding</Link>
+            <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
         </nav>
     )
 }
