0beaf882bced4854fd223f1d38801da874ef2d42
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 09:35:56 +0545

    gap above for children added

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
+        <div className="max-w-3xl mx-auto p-4">
+          <Navbar/>
+          <div className='mt-8'>
+          {children}
+          </div>
+        </div>
         </body>
     </html>
   )
