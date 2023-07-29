897394757ef5744ccde1804518b3a9fb98ec1fdd
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 09:36:56 +0545

    added editTopic component

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,9 @@
+import EditTopicForm from "@/components/EditTopicForm";
+
+export default function EditTopic() {
+    return (
+        <div>
+            <EditTopicForm />
+        </div>
+    )
+}
