ff13e51530ce3f48148ab3e5de56e6dd0a4dd68c
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 09:38:50 +0545

    added topic model

---
+++ models/topic.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose";
+
+const topicSchema = new Schema(
+    {
+        title: String,
+        description: String,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
+
+export default Topic;
+
