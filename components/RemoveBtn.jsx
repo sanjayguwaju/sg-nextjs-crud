51717a85e5e5bfe5e7c897fd01873e221c91b781
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 12:07:21 +0545

    remove button working successfully

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,8 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
 import {HiOutlineTrash} from 'react-icons/hi'
 
-export default function RemoveBtn() {
+export default function RemoveBtn({id}) {
+    const router = useRouter();
+    const removeTopic = async () => {
+        const confirmed = confirm("Are you sure you want to delete this topic?");
+        if (confirmed) {
+            try {
+                const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+                    method: "DELETE",
+                });
+                if (!res.ok) {
+                    throw new Error("Failed to delete topic");
+                }
+                if (res.ok) {
+                    router.refresh();
+                }
+            } catch (error) {
+                console.log("Error deleting topic", error);
+            }
+        }
+    }
+
     return (
-        <button className='text-red-400'>
+        <button onClick={removeTopic} className='text-red-400'>
             <HiOutlineTrash size={24}/>
         </button>
     )
