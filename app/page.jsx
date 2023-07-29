0304e137ebec185ef609add3d631dabf3fc9c5cf
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 09:36:13 +0545

    added topic list component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import TopicsList from "@/components/TopicsList"
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <TopicsList />
+    </>
   )
 }
