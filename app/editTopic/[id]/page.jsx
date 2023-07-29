95b3c99ad0489978efa5af28d9ab81afe70ba821
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 12:06:42 +0545

    implemented the getTopic by id for update

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,9 +1,29 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function EditTopic() {
+const getTopicById = async (id) => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+            cache: "no-cache",
+        });
+    
+        if(!res.ok) {
+            throw new Error("Failed to fetch topic");
+        }
+        return res.json();
+        
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export default async function EditTopic({params}) {
+    const {id} = params;
+    const {topic} = await getTopicById(id);
+    const {title, description} = topic;
+
     return (
         <div>
-            <EditTopicForm />
+            <EditTopicForm id={id} title={title} description={description} />
         </div>
     )
 }
