5d665eb3764a3a04054a950856db2cbfcf67de30
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-29 12:07:36 +0545

    get topic list to the main page successfully

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,22 +2,44 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import {HiPencilAlt} from "react-icons/hi";
 
-export default function TopicsList() {
+const getTopic = async () => {
+
+    try {
+        const res = await fetch(`http://localhost:3000/api/topics` , {
+            cache: "no-store",
+        });
+    
+        if (!res.ok) {
+            throw new Error("Failed to fetch Topics");
+        } 
+        return res.json();
+        
+    } catch (error) {
+        console.log("Error fetching topics", error.message);
+    }
+};
+
+export default async function TopicsList() {
+
+    const {topics} = await getTopic();
+    console.log(topics);
+
     return (
         <>
-            <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-                <div>
-                    <h2 className="font-bold text-2xl">Topics List</h2>
-                    <div>Topic description</div>
+            {topics.map((t) => (
+                <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+                    <div>
+                        <h2 className="font-bold text-2xl">{t.title}</h2>
+                        <div>{t.description}</div>
+                    </div>
+                    <div className="flex gap-2">
+                        <RemoveBtn id={t._id} />
+                        <Link href={`/editTopic/${t._id}`}>
+                            <HiPencilAlt  size={24}/>
+                        </Link>
+                    </div>
                 </div>
-                <div className="flex gap-2">
-                    <RemoveBtn />
-                    <Link href={"/editTopic/123"}>
-                        <HiPencilAlt  size={24}/>
-                    </Link>
-                </div>
-            </div>
-
-        </>
+            ))}
+        </> 
     )
 }
