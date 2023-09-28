5069fc2f3c8f165883496bf76c082580bba88d08
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-28 13:51:35 +0545

    fix: typo changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-            <Link className="text-white font-bold" href={"/"}>GT Coding</Link>
+            <Link className="text-white font-bold" href={"/"}>Sanjay Note App</Link>
             <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
         </nav>
     )
